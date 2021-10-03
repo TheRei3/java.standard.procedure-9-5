b92d18d70bae46263933010f7c6cad946e984dcb
admin <admin@host.docker.internal>
2021-10-04 08:04:43 +0900

    ex9no5

---
+++ ex9no5/src/ex9no5/ex9no5.java
@@ -0,0 +1,14 @@
+package ex9no5;
+
+public class ex9no5 {  //  455p
+	public static void main(String[] args) {
+		String str = new String("KOERA");
+		java.util.Date today = new java.util.Date();
+		
+		System.out.println(str);
+		System.out.println(str.toString());
+		System.out.println(today);
+		System.out.println(today.toString());
+	}
+
+}
